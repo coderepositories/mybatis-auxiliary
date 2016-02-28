5e5c543daa6f2134b8aef30c64ee936e6a9a998e
zhangguangyong <1243610991@qq.com>
2016-02-28 23:31:39 +0800

    修改部署发布脚本-deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@
 echo "set version"
 mvn versions:set -DnewVersion=1.0.0-SNAPSHOT
 
+echo "commit version"
+mvn versions:commit
+
 echo "deploy to oss"
 mvn clean deploy -P release -Dgpg.passphrase=12345678
