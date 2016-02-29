432f383eae7733192504e22c271f4e508f8f3a17
zhangguangyong <1243610991@qq.com>
2016-02-29 11:11:55 +0800

    修改deploy.sh脚本，使其支持动态更新版本部署发布

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,14 @@
 echo "set version"
 mvn versions:set -DnewVersion=1.0.0-SNAPSHOT
 
+echo "sleep 3s"
+sleep 3
+
 echo "commit version"
 mvn versions:commit
+
+echo "sleep 3s"
+sleep 3
 
 echo "deploy to oss"
 mvn clean deploy -P release -Dgpg.passphrase=12345678
--- mybatis-auxiliary-core/src/main/java/com/github/coderepositories/App.java
+++ mybatis-auxiliary-core/src/main/java/com/github/coderepositories/App.java
@@ -1,13 +1,16 @@
 package com.github.coderepositories;
 
+import com.github.coderepositories.jcommons.core.X;
+
+import java.util.Date;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String dateString = X.dateFormat(new Date(), "yyyy-MM-dd");
+        System.out.println(dateString);
+        System.out.println("Hello World!");
     }
 }
