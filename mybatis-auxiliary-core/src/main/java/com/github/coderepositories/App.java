f15dfb2fdce5a4162bde587c62cb28018d7a0ba7
zhangguangyong <1243610991@qq.com>
2016-02-29 11:23:38 +0800

    修改App.java

--- mybatis-auxiliary-core/src/main/java/com/github/coderepositories/App.java
+++ mybatis-auxiliary-core/src/main/java/com/github/coderepositories/App.java
@@ -1,16 +1,11 @@
 package com.github.coderepositories;
 
-import com.github.coderepositories.jcommons.core.X;
-
-import java.util.Date;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        String dateString = X.dateFormat(new Date(), "yyyy-MM-dd");
-        System.out.println(dateString);
+        
         System.out.println("Hello World!");
     }
 }
