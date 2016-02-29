afba3974b59ae7c56ad1c52caeb02d13fc94de78
zhangguangyong <1243610991@qq.com>
2016-03-01 00:10:27 +0800

    修改发布脚本-deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+
+#current date time
+current=`date "+%Y%m%d%H%M%S"`
+
 echo "set version"
-mvn versions:set -DnewVersion=1.0.0-SNAPSHOT
+mvn versions:set -DnewVersion=${current}
 
 echo "sleep 3s"
 sleep 3
@@ -12,4 +16,5 @@ echo "sleep 3s"
 sleep 3
 
 echo "deploy to oss"
+
 mvn clean deploy -P release -Dgpg.passphrase=12345678
