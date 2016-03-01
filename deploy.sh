123abe2cfa6bbcad94d13a2e5d33d7798237815a
zhangguangyong <1243610991@qq.com>
2016-03-01 11:39:28 +0800

    使用maven命令来指定项目的版本

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
+# current date time
+# current=`date "+%Y%m%d%H%M%S"`
 
-#current date time
-current=`date "+%Y%m%d%H%M%S"`
+# echo "set version"
+# mvn versions:set -DnewVersion=${current}
 
-echo "set version"
-mvn versions:set -DnewVersion=${current}
+# echo "sleep 3s"
+# sleep 3
 
-echo "sleep 3s"
-sleep 3
+# echo "commit version"
+# mvn versions:commit
 
-echo "commit version"
-mvn versions:commit
-
-echo "sleep 3s"
-sleep 3
+# echo "sleep 3s"
+# sleep 3
 
 echo "deploy to oss"
-
 mvn clean deploy -P release -Dgpg.passphrase=12345678
