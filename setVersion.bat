4096fbce52ef2a53c53f24156a08a9967a702860
zhangguangyong <1243610991@qq.com>
2016-03-04 10:44:48 +0800

    更改设置版本脚本

--- setVersion.bat
+++ setVersion.bat
@@ -1,13 +1,23 @@
 @echo off
 
-:: read version
-echo "read version"
+:: read current version
 set /p v=<currentVersion
-set /a r = %v% + 1
-echo %r%
 
-:: set version
-:: mvn versions:set -DnewVersion=%v%
-:: mvn versions:commit
+:: calculate new version
+set v1=%v:~0,1%
+set v2=%v:~1,1%
+set v3=%v:~2,1%
+set /a newVersion=v1*100+v2*10+v3+1
 
+:: write new version to currentVersion file
+echo %newVersion% > currentVersion
 
+:: build new version
+set v1=%newVersion:~0,1%
+set v2=%newVersion:~1,1%
+set v3=%newVersion:~2,1%
+set pomVersion=%v1%.%v2%.%v3%
+
+:: mvn version set
+mvn versions:set -DnewVersion=%pomVersion%
+mvn versions:commit
